9aea3dbadaf1c8edd3dca8eab343e71f0cb094c0
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2024-12-25 16:16:48 +0700

    final-24122024

--- font_end/web_quanao/src/redux/user/authSelectors.jsx
+++
@@ -1,3 +0,0 @@
-export const selectUser = (state) => state.auth.user;
-export const selectIsLoading = (state) => state.auth.isLoading;
-export const selectError = (state) => state.auth.error;
--- font_end/web_quanao/src/redux/user/loginSelectors.jsx
+++
@@ -1,4 +0,0 @@
-export const selectAuthStatus = (state) => state.auth.status;
-export const selectAuthUser = (state) => state.auth.user;
-export const selectAuthToken = (state) => state.auth.token;
-export const selectAuthError = (state) => state.auth.error;
